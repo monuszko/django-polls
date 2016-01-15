540b20e9193c5b32638466d5b13c0708eacf7581
tdwizard.com <support@tdwizard.com>
2016-01-15 21:03:58 +0100

    visible field in polls model

--- polls/models.py
+++ polls/models.py
@@ -7,6 +7,7 @@ from django.utils import timezone
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    visible = models.NullBooleanField()
 
     def __unicode__(self):  # Python 3: def __str__(self):
         return self.question
