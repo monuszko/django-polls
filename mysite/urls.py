8fcd820223bfb8ba4057ce85f3f4e923d76a39bc
Marek Onuszko <marek.onuszko@gmail.com>
2016-01-31 17:43:37 +0100

    On a second thought, I'm going to need the project directory.

---
+++ mysite/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^polls/', include('polls.urls', namespace="polls")),
+    url(r'^accounts/', include('registration.backends.default.urls')),
+    url('^', include('django.contrib.auth.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
